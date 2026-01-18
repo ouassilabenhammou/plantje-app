6799c81cfe06a5ca336ea12011dd3048343ac885
ouassilabenhammou <509114@student.fontys.nl>
2026-01-18 14:13:04 +0100

    feat(theme): fonts toegevoegd

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,11 +6,22 @@ import { PaperProvider } from "react-native-paper";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { useFonts } from "expo-font";
+
 export const unstable_settings = {
   anchor: "(tabs)",
 };
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    "Inter-Regular": require("../assets/fonts/Inter-Regular.ttf"),
+    "Inter-Medium": require("../assets/fonts/Inter-Medium.ttf"),
+    "Inter-SemiBold": require("../assets/fonts/Inter-SemiBold.ttf"),
+    "Inter-Bold": require("../assets/fonts/Inter-Bold.ttf"),
+  });
+
+  if (!loaded) return null; // voorkomt flikkeren
+
   return (
     <PaperProvider>
       <SafeAreaView style={styles.safe}>
