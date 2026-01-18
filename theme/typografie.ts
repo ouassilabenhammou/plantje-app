0946ecc846aa9cb9ad54a98ac1e6d3767ec771a8
ouassilabenhammou <509114@student.fontys.nl>
2026-01-18 13:49:40 +0100

    feat(theme): typografie toegevoegd

---
+++ theme/typografie.ts
@@ -0,0 +1,53 @@
+export type Typography = {
+  pageTitle: {
+    fontSize: number;
+    fontWeight: "600";
+  };
+  sectionTitle: {
+    fontSize: number;
+    fontWeight: "500";
+  };
+  body: {
+    fontSize: number;
+    fontWeight: "400";
+  };
+  subText: {
+    fontSize: number;
+    fontWeight: "400";
+  };
+  label: {
+    fontSize: number;
+    fontWeight: "500";
+  };
+  navbar: {
+    fontSize: number;
+    fontWeight: "500";
+  };
+};
+
+export const typography: Typography = {
+  pageTitle: {
+    fontSize: 25,
+    fontWeight: "600",
+  },
+  sectionTitle: {
+    fontSize: 19,
+    fontWeight: "500",
+  },
+  body: {
+    fontSize: 16,
+    fontWeight: "400",
+  },
+  subText: {
+    fontSize: 14,
+    fontWeight: "400",
+  },
+  label: {
+    fontSize: 12,
+    fontWeight: "500",
+  },
+  navbar: {
+    fontSize: 11,
+    fontWeight: "500",
+  },
+};
